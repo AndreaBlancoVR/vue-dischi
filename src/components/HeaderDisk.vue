1ea2872e349ed21b8ddbd92ba4d0da0d4040b79b
andreablancovr@gmail.com <andreablancovr@gmail.com>
2022-03-11 00:46:21 +0100

    Header completato

--- src/components/HeaderDisk.vue
+++ src/components/HeaderDisk.vue
@@ -1,18 +1,40 @@
 <template>
-  <div>
-    Header
-  </div>
+  <header class="header">
+    <figure class="logo">
+      <img :src="require('../assets/img/' + logo) ">
+    </figure>
+  </header>
 </template>
 
 <script>
-export default {
-  name: 'HeaderDisk',
-  props: {
-    
+  export default {
+    name: 'HeaderDisk',
+    props: {
+      logo: {
+        type: String,
+        default: 'logo-small.svg'
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
+  .header {
+    width: 100%;
+    height: 70px;
+    background-color: #2E3A46;
+    border: 1px solid red;
+
+    .logo {
+      width: 45px;
+      height: 45px;
+      margin: 12px 23px;
+
+      // img {
+      //   color: #1ED760;
+      // }
+    }
+  }
 
 </style>
